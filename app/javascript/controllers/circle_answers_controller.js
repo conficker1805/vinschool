18608eac8d6d5bae360c0c1f64b3dfd56f259cf5
Hai Luu <hailuu@yojee.com>
2025-04-13 14:00:54 +0700

    Add new answer type

---
+++ app/javascript/controllers/circle_answers_controller.js
@@ -0,0 +1,111 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["template", "answer", "submit"]
+
+  connect() {
+    // Add event when user clicks on an answer
+    // this.answerTargets.forEach(answer => {
+    //   answer.addEventListener("click", this.toggleAnswer.bind(this));
+    // });
+
+    // this.setDefaultBlank()
+    // this.updateSubmitButtonState()
+    this.loadSounds()
+  }
+
+  verify() {
+    let result = true
+
+    this.answerTargets.map(input => {
+      const isCorrect = input.dataset.correct == 'true'
+      const selected = input.checked
+
+      if (input.checked != isCorrect) {
+        result = false
+      }
+    });
+
+    this.playSound(result);
+    this.highlightCorrectAnswers()
+    // this.addReloadButton();
+
+    // const klass = result ? 'correct' : 'incorrect'
+    // document.querySelector(".title").classList.add(klass);
+
+    // // this.submitTarget.textContent = 'Câu tiếp theo'
+    // this.answerTargets.forEach(answerTarget => answerTarget.disabled = true);
+  }
+
+  loadSounds() {
+    // This make the sound open faster on mobile web
+    this.correctAudio = new Audio("/sounds/correct.wav");
+    this.incorrectAudio = new Audio("/sounds/incorrect.wav");
+    this.correctAudio.load();
+    this.incorrectAudio.load();
+  }
+
+  playSound(isCorrect) {
+    const sound = isCorrect ? this.correctAudio : this.incorrectAudio;
+    sound.play()
+  }
+
+  // updateSubmitButtonState() {
+  //   const hasChecked = this.answerTargets.some(input => input.checked)
+  //   this.submitTarget.disabled = !hasChecked
+  // }
+
+  highlightCorrectAnswers() {
+    this.answerTargets.forEach(input => {
+      const isCorrect = input.dataset.correct == 'true'
+
+      if (isCorrect)
+        input.nextElementSibling.classList.add("highlight")
+    });
+  }
+
+  // toggleAnswer(event) {
+  //   const templateData = JSON.parse(this.templateTarget.dataset.template)
+  //   const input = event.currentTarget;
+
+  //   // console.log('templateData:', templateData);
+
+  //   if (templateData.answer_type == 'single_choice')
+  //     this.uncheckOtherAnswers(input)
+
+  //   if (input.checked)
+  //     this.setValueForBlank(input.nextElementSibling.textContent)
+  //   else
+  //     this.setDefaultBlank()
+
+  //   this.updateSubmitButtonState()
+  // }
+
+  // uncheckOtherAnswers(input) {
+  //   if (!input.checked) return
+
+  //   this.answerTargets.forEach(target => {
+  //     if (target !== input) {
+  //       target.checked = false;
+  //     }
+  //   });
+  // }
+
+  // setValueForBlank(value) {
+  //   const elm = document.querySelector("[data-replace]")
+  //   if (elm) { elm.textContent = value; }
+  // }
+
+  // setDefaultBlank() {
+  //   const elm = document.querySelector("[data-replace]")
+  //   if (elm) { elm.textContent = elm.getAttribute("data-replace") }
+  // }
+
+  // addReloadButton() {
+  //   const newButton = document.createElement("button")
+  //   newButton.className = "btn btn-primary w-100"
+  //   newButton.textContent = "Câu tiếp theo"
+  //   newButton.addEventListener("click", () => window.location.reload())
+  //   this.submitTarget.replaceWith(newButton)
+  // }
+}
--- db/seeds/grade1_math/chapter6_time.seeds.rb
+++ db/seeds/grade1_math/chapter6_time.seeds.rb
@@ -16,7 +16,7 @@ question_template = QuestionTemplate.create!(
   TEXT
 )
 
-10.times.each do
+5.times.each do
   origin = %w[giây phút giờ ngày tuần tháng năm]
   results = origin - origin.sample(2)
   time_values = results.shuffle
@@ -30,7 +30,7 @@ question_template = QuestionTemplate.create!(
   )
 end
 
-10.times.each do
+5.times.each do
   origin =  %w[năm tháng tuần ngày giờ phút giây]
   results = origin - origin.sample(2)
   time_values = results.shuffle
@@ -43,3 +43,74 @@ end
     },
   )
 end
+
+# Tìm đơn vị thời gian DÀI HƠN X
+question_template = QuestionTemplate.create!(
+  grade: 1,
+  subject: :math,
+  chapter: 5,
+  question_type: :time_larger_shorter,
+  answer_type: :circle_answers,
+  slim_content: <<~TEXT
+    .title Khoanh tròn đáp án đúng
+    p = 'Đơn vị thời gian dài hơn ' + @question.options['milestone'].upcase + ' là:'
+    .answers.d-flex.gap-5
+      - @question.options['answers'].each_with_index do |a, idx|
+        input type="checkbox" id=idx class="circle-btn d-none" data-circle-answers-target="answer" data-correct=a['correct'].to_s
+        label.circlable.d-inline for=idx = a['text']
+  TEXT
+)
+
+5.times do
+  origin =  %w[giây phút giờ ngày tuần tháng năm]
+  milestone = origin[1..-2].sample
+  idx = origin.index(milestone)
+  smaller = origin[0...idx].map { |time| { text: time, correct: false } }
+  larger = origin[(idx + 1)..].map { |time| { text: time, correct: true } }
+
+  answers = loop do
+    arr = (smaller + larger).shuffle.first(4)
+    break arr if arr.pluck(:correct).any?(true)
+  end
+
+  Question.create!(
+    question_template:,
+    options: { milestone:, answers: },
+  )
+end
+
+# Tìm đơn vị thời gian NGẮN HƠN X
+question_template = QuestionTemplate.create!(
+  grade: 1,
+  subject: :math,
+  chapter: 5,
+  question_type: :time_larger_shorter,
+  answer_type: :circle_answers,
+  slim_content: <<~TEXT
+    .title Khoanh tròn đáp án đúng
+    p = 'Đơn vị thời gian ngắn hơn ' + @question.options['milestone'].upcase + ' là:'
+    .answers.d-flex.gap-5
+      - @question.options['answers'].each_with_index do |a, idx|
+        input type="checkbox" id=idx class="circle-btn d-none" data-circle-answers-target="answer" data-correct=a['correct'].to_s
+        label.circlable.d-inline for=idx = a['text']
+  TEXT
+)
+
+5.times do
+  origin =  %w[giây phút giờ ngày tuần tháng năm]
+  milestone = origin[1..-2].sample
+  idx = origin.index(milestone)
+  smaller = origin[0...idx].map { |time| { text: time, correct: true } }
+  larger = origin[(idx + 1)..].map { |time| { text: time, correct: false } }
+
+  answers = loop do
+    arr = (smaller + larger).shuffle.first(4)
+    break arr if arr.pluck(:correct).any?(true)
+  end
+
+  Question.create!(
+    question_template:,
+    options: { milestone:, answers: },
+  )
+end
+
