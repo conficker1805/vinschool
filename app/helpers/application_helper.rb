91d568c17d1741e03bb95a51a8c5fb58b861c0e6
Hai Luu <hailuu@yojee.com>
2025-04-13 23:49:06 +0700

    Setup minio for local

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,18 @@
 module ApplicationHelper
   def aws_shared_url(file_path)
-    fog_connection = Fog::Storage.new(
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-southeast-1'
-    )
+    if Rails.env.production?
+      fog_connection = Fog::Storage.new(
+        provider: 'AWS',
+        aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+        aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+        region: 'ap-southeast-1'
+      )
 
-    # Truy cập đối tượng S3 mà bạn muốn tạo presigned URL
-    file = fog_connection.directories.get('vinschool-one').files.get(file_path)
-
-    # Tạo presigned URL với thời gian hết hạn (ví dụ: 1 giờ)
-    file.url(Time.now + 60)
+      file = fog_connection.directories.get('vinschool-one').files.get(file_path)
+      file.url(Time.now + 60)
+    else
+      credentials = Rails.application.credentials
+      [credentials.aws[:endpoint], credentials.aws[:bucket], file_path].join('/')
+    end
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,33 @@
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: Rails.application.credentials.aws[:region],
-    # endpoint: Rails.application.credentials.aws[:endpoint],
-    force_path_style: true,
-    signature_version: 'v4',
-  }
-    config.fog_directory  = Rails.application.credentials.aws[:bucket]
-    config.fog_public     = true
-    config.fog_attributes = { 'Cache-Control' => "max-age=#{7.days.to_i}" }
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: Rails.application.credentials.aws[:region],
+      force_path_style: true,
+      signature_version: 'v4',
+    }
+      config.fog_directory  = Rails.application.credentials.aws[:bucket]
+      config.fog_public     = true
+      config.fog_attributes = { 'Cache-Control' => "max-age=#{7.days.to_i}" }
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      access_key_id: Rails.application.credentials.aws[:access_key_id],
+      secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: Rails.application.credentials.aws[:region],
+      endpoint: Rails.application.credentials.aws[:endpoint],
+      force_path_style: true,
+      signature_version: 'v4',
+    }
+      config.fog_directory  = Rails.application.credentials.aws[:bucket]
+      config.fog_public     = true
+      config.fog_attributes = { 'Cache-Control' => "max-age=#{7.days.to_i}" }
+  end
 end
