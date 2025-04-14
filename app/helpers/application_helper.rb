c38020f6b01e13102d64a0e10bc910bc237e68ba
Hai Luu <hailuu@yojee.com>
2025-04-14 14:15:01 +0700

    Cache images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,17 @@
 module ApplicationHelper
   def aws_shared_url(file_path)
     if Rails.env.production?
-      fog_connection = Fog::Storage.new(
-        provider: 'AWS',
-        aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-        aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-        region: 'ap-southeast-1'
-      )
+      Rails.cache.fetch("s3_url:#{file_path}", expires_in: 55.minutes) do
+        fog_connection = Fog::Storage.new(
+          provider: 'AWS',
+          aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+          aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+          region: 'ap-southeast-1'
+        )
 
-      file = fog_connection.directories.get('vinschool-one').files.get(file_path)
-      file.url(Time.now + 60)
+        file = fog_connection.directories.get('vinschool-one').files.get(file_path)
+        file.url(1.hour.from_now)
+      end
     else
       credentials = Rails.application.credentials
       [credentials.aws[:endpoint], credentials.aws[:bucket], file_path].join('/')
